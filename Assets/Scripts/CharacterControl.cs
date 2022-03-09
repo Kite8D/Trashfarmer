ee6951c0527fe68b73d5bce9689651c7ec3999b5
Joonas Aro <aro.joonas@gmail.com>
2022-03-09 19:00:32 +0200

    CharacterControl updated
    Character can now be moved with joystic or tapping the screen.
    Using joystick counts as tapping the screen, needs a fix.

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -6,61 +6,123 @@ using UnityEngine.InputSystem;
 namespace Trashfarmer
 {
 
-public class CharacterControl : MonoBehaviour
-{
-    [SerializeField]
-    private float velocity = 1;
-    private Animator animator;
-    private new SpriteRenderer renderer;
-    private Vector2 moveInput;
-
-	private void Awake()
-	{
-        animator = GetComponent<Animator>();
-        if (animator == null)
+    public class CharacterControl : MonoBehaviour
+    {
+        public enum ControlState
 		{
-            Debug.LogError("Character is missing an animator component!");
-            Debug.Break();
+            GamePad,
+            Touch
 		}
 
-        renderer = GetComponent<SpriteRenderer>();
-        if (renderer == null)
-		{
-            Debug.LogError("Character is missing an renderer component!");
-            Debug.Break();
+        [SerializeField]
+        private float velocity = 1;
+        private Animator animator;
+        private new SpriteRenderer renderer;
+        private Vector2 moveInput;
+        private Vector2 touchPosition;
+        private Vector2 targetPosition;
+        private ControlState controlState = ControlState.GamePad;
+
+	    private void Awake()
+	    {
+            animator = GetComponent<Animator>();
+            if (animator == null)
+		    {
+                Debug.LogError("Character is missing an animator component!");
+                Debug.Break();
+		    }
+
+            renderer = GetComponent<SpriteRenderer>();
+            if (renderer == null)
+		    {
+                Debug.LogError("Character is missing an renderer component!");
+                Debug.Break();
+            }
+	    }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MoveCharacter();
+            UpdateAnimator();
         }
-	}
-	void Start()
-    {
-        
+
+        private void UpdateAnimator()
+	    {
+            renderer.flipX = moveInput.x < 0;
+
+            animator.SetFloat("speed", moveInput.magnitude);
+            animator.SetFloat("horizontal", moveInput.x);
+            animator.SetFloat("vertical", moveInput.y);
+        }
+
+        private void MoveCharacter()
+        {
+            switch (controlState)
+            {
+                case ControlState.GamePad:
+                    Vector2 movement = moveInput * Time.deltaTime * velocity;
+                    // Transform property allows us to read and manipulate GameObject's position
+                    // in the game world.
+                    transform.Translate(movement);
+                    break;
+                case ControlState.Touch:
+                    // Koska Vector2:sta ei voi vähentää Vector3:a, pitää suorittaa tyyppimuunnos
+                    Vector2 travel = targetPosition - (Vector2)transform.position;
+
+                    // Normalisointi muuntaa vektorin pituuden yhdeksi
+                    Vector3 frameMovement = travel.normalized * velocity * Time.deltaTime;
+
+                    // Magnitude palauttaa vektorin pituuden. Tässä vektorin pituus kuvaa
+                    // jäljellä olevaa matkaa
+                    float distance = travel.magnitude;
+                    
+                    if (frameMovement.magnitude < distance)
+					{
+                        // Matkaa on vielä jäljellä, kuljetaan kohti kohdepistettä
+                        transform.Translate(frameMovement);
+                    }
+					else
+					{
+                        // Päämäärä saavutettu
+                        transform.position = targetPosition;
+                        moveInput = Vector2.zero;
+					}
+
+                    break;
+            }
+	    }
+
+        private void OnMove(InputAction.CallbackContext callbackContext)
+	    {
+            controlState = ControlState.GamePad;
+            moveInput = callbackContext.ReadValue<Vector2>();
+	    }
+
+        private void OnTap(InputAction.CallbackContext context)
+		{
+            Debug.Log("Tap!");
+            Debug.Log(context.phase);
+		}
+
+        private void OnTouchPosition(InputAction.CallbackContext contextyolo)
+		{
+            controlState = ControlState.Touch;
+
+            this.touchPosition = contextyolo.ReadValue<Vector2>();
+
+            // Muunnetaan 2D koorinaatti 3D-koordinaatistoon
+            Vector3 screenCoordinate = new Vector3(touchPosition.x, touchPosition.y, 0);
+
+            // Muunnetaan näytön koordinaatti pelimaailman koordinaatistoon
+            Vector3 worldCoordinate = Camera.main.ScreenToWorldPoint(screenCoordinate);
+
+            // Muunnetaan maailmankoordinaatti 2D-koordinaatistoon. HUOM! implisiittinen
+            // tyyppimuunnos Vector3:sta -> Vector2:seen
+            targetPosition = worldCoordinate;
+
+            moveInput = (targetPosition - (Vector2)transform.position).normalized;
+		}
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveCharacter();
-        UpdateAnimator();
-    }
-
-    private void UpdateAnimator()
-	{
-        renderer.flipX = moveInput.x < 0;
-
-        animator.SetFloat("speed", moveInput.magnitude);
-        animator.SetFloat("horizontal", moveInput.x);
-        animator.SetFloat("vertical", moveInput.y);
-    }
-
-    private void MoveCharacter()
-	{
-        Vector2 movement = moveInput * Time.deltaTime * velocity;
-        transform.Translate(movement);
-	}
-
-    private void OnMove(InputAction.CallbackContext callbackContext)
-	{
-        moveInput = callbackContext.ReadValue<Vector2>();
-	}
-}
 
 }
