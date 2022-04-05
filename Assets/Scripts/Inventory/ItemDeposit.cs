7b651de6e47bdeec44889186dca03a7034574601
Vertti Parviainen <vertzu@windowslive.com>
2022-04-05 21:54:11 +0300

    Item Deposit script

---
+++ Assets/Scripts/Inventory/ItemDeposit.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InventorySystem.Visual;
+
+namespace InventorySystem
+{
+    public class ItemDeposit : MonoBehaviour
+    {
+        public ItemType takeItem;
+    }
+}
