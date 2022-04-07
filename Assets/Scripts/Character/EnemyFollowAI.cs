84b18356037dd9013c7631b0353cc8913b5b4937
Vertti Parviainen <vertzu@windowslive.com>
2022-04-07 16:02:32 +0300

    Enemy backs off to starting position
    When player leaves the area

--- Assets/Scripts/Character/EnemyFollowAI.cs
+++ Assets/Scripts/Character/EnemyFollowAI.cs
@@ -12,35 +12,38 @@ namespace Trashfarmer
         private Transform target;
 
         public DangerZoneDetector dzd;
+        private Vector2 startPosition;
         
+
+        void Start()
+        {
+            startPosition = transform.position;
+        }
 
         //Scripti tunnistaa pelaajaan tagista: Player
         void OnTriggerEnter2D(Collider2D other)
         {
-            if(dzd.PlayerInArea == true)
-                {
-                    Debug.Log("Enemy found you");
-                    target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-                }
+            if (dzd.PlayerInArea == true)
+            {
+                Debug.Log("Enemy found you");
+                target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            }
         }
 
         //komento jolla otus jahtaa pelaajaa 
         void OnTriggerStay2D(Collider2D other)
         {
-            if(dzd.PlayerInArea == true)
-                {
-                    Debug.Log("Enemy started to move");
-                    transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-                }
-        }
-
-        //Tästä uupuu vielä komento, jolla otus palaa aloitus kohtaan
-        void OnTriggerExit2D(Collider2D other)
-        {
-            if(dzd.PlayerInArea == false)
-                {
-                    Debug.Log("Enemy backing up");
-                }
+            if (dzd.PlayerInArea == true)
+            {
+                Debug.Log("Enemy started to move");
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            } 
+            // Peruuttaa aloituspaikkaan, kun pelaaja ei ole alueella
+            else if (dzd.PlayerInArea == false)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
+                Debug.Log("Enemy backing up");
+            }
         }
     }
 }
